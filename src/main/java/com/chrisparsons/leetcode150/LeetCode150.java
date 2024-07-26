c5b3e1d34476523ccdc570080dac5e3e2098e5b9
unknown <chriseparsons@hotmail.co.uk>
2024-07-26 13:37:12 +0100

    Update pushed.

--- src/main/java/com/chrisparsons/leetcode150/LeetCode150.java
+++ src/main/java/com/chrisparsons/leetcode150/LeetCode150.java
@@ -426,9 +426,7 @@ public class LeetCode150 {
      *
      */
 
-    public int jump(int[] nums) {
-
-        //TODO
+    public int jump(int[] nums) { //TODO
 
         if(nums.length == 1) {
 
@@ -1268,6 +1266,60 @@ public class LeetCode150 {
     }
 
 
+    /**
+     *
+     * 17. Letter Combinations of a Phone Number
+     *
+     *
+     * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+     *
+     * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+     *
+     *
+     *
+     * Example 1:
+     *
+     * Input: digits = "23"
+     * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+     *
+     * Example 2:
+     *
+     * Input: digits = ""
+     * Output: []
+     *
+     * Example 3:
+     *
+     * Input: digits = "2"
+     * Output: ["a","b","c"]
+     *
+     *
+     *
+     * Constraints:
+     *
+     *     0 <= digits.length <= 4
+     *     digits[i] is a digit in the range ['2', '9'].
+     *
+     *
+     *
+     */
+
+    public List<String> letterCombinations(String digits) {
+
+        Map<Integer, String[]> map = Map.of(
+                2, new String[]{"a","b","c"},
+                3, new String[]{"d","e","f"},
+                4, new String[]{"g","h","i"},
+                5, new String[]{"j","k","l"},
+                6, new String[]{"m","n","o"},
+                7, new String[]{"p","q","r","s"},
+                8, new String[]{"t","u","v"},
+                9, new String[]{"w","x","y","z"}
+        );
+
+        //TODO
+
+        return List.of("");
+    }
 
     /**
      *
